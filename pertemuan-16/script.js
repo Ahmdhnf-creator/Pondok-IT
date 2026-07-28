5fd34adfb2d3cd2a6f48fbcfbfbb8b02347a1441
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 08:07:44 +0700

    variabel & expression

---
+++ pertemuan-16/script.js
@@ -0,0 +1,14 @@
+/*
+Nama    : Budi
+Kelas   : 6
+Tanggal : Senin, 29 Maret 2026*/
+
+//variabel biodata
+const Nama = "Budi";
+const Kelas = "6";
+const Tanggal = "Senin, 29 Maret 2026";
+
+//console.log biodata
+console.log(Nama)
+console.log(Kelas)
+console.log(Tanggal)
---
+++ pertemuan-16/statement-expression.js
@@ -0,0 +1,7 @@
+//expression
+
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(8 / 2);
+console.log(2 * 2);
+console.log("hallo" + Nama)
